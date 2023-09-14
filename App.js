66f2ce81ba8e07bf8a10c9bb0d7762b5efca2e70
Abhirwt123 <pardeeprawat1617@gmail.com>
2023-09-14 20:03:43 +0530

    chapter01

---
+++ App.js
@@ -0,0 +1,12 @@
+const parent = React.createElement("div", { id: "parent" }, [
+    React.createElement("div", { id: "child" }, [
+        React.createElement("h1", {}, "hello i am h1"),
+        React.createElement("h2", {}, "hello i am h1"),
+    ]),
+    React.createElement("div", { id: "child2" }, [
+        React.createElement("h1", {}, "hello i am h1"),
+        React.createElement("h2", {}, "hello i am h1"),
+    ]),
+]);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
